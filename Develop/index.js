12693b32ee7063f7c08943fa4ce31ae2cf23d170
Brittny Rosenow <brittnyrosenow@gmail.com>
2020-09-03 12:54:42 -0600

    updated to generate

---
+++ Develop/index.js
@@ -0,0 +1,73 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const path = require('path');
+
+const generateMarkdown = require("./utils/generateMarkdown")
+
+// array of questions for user
+const questions = [
+
+        {
+          type: "input",
+          message: "What is your GitHub username?", 
+          name: "username" 
+        },
+        {
+          type: "input",
+          message: "What is your email address?", 
+          name: "email" 
+        },
+        {
+            type: "input",
+            message: "What is your project title?", 
+            name: "title" 
+          },
+          {
+            type: "input",
+            message: "Add a short description of your project.", 
+            name: "description" 
+          },
+          {
+            type: "input",
+            message: "Enter your installation instructions.", 
+            name: "installation" 
+          },
+          {
+            type: "input",
+            message: "Enter your usage information.", 
+            name: "usage" 
+          },
+          {
+            type: "input",
+            message: "Enter your contribution guidelines.", 
+            name: "contribution" 
+          },
+          {
+            type: "input",
+            message: "Enter your testing instructions.", 
+            name: "testing" 
+          },
+        {
+          type: "list",
+          message: "Select the appropriate license for your project (use arrow keys).",
+          name: "license",
+          choices: ["Apache 2.0", "MIT", "ISC", "GNU GPLv3", "None"]
+        }
+
+];
+
+
+// function to write README file
+function writeToFile(fileName, data) {
+
+return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+
+}
+// function to initialize program
+function init() {
+  inquirer.prompt(questions).then(data => writeToFile("README.md", generateMarkdown(data)));
+}
+
+// function call to initialize program
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,53 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+
+    var apache = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    var mit = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    var isc = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+    var gnu3 = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    var badge = "";
+    
+    if (data.license === "Apache 2.0") {
+      badge = apache;
+    } else if (data.license === "MIT") {
+      badge = mit;
+    } else if (data.license === "ISC") {
+      badge = isc;
+    } else if (data.license === "GNU GPLv3") {
+      badge = gnu3;
+    } else {
+      badge = "";
+    };
+      return `# ${data.title}
+      ## Table of Contents
+      * [Badge](#badge)
+      * [Description](#description)
+      * [Installation](#installation)
+      * [Usage](#usage)
+      * [Licensing](#licensing)
+      * [Contribution](#contribution)
+      * [Testing](#testing)
+      * [Questions](#questions)
+      ## Badge
+      ${badge}
+      ## Description
+      ${data.description}
+      
+      ## Installation
+      ${data.installation}
+      
+      ## Usage
+      ${data.usage}
+      ## Licensing
+      ${data.license} 
+      ## Contribution
+      ${data.contribution}
+      
+      ## Testing
+      ${data.testing}
+      
+      ## Questions
+      Contact me at ${data.email} or visit my profile at https://github.com/${data.username}`;
+    }
+    
+    module.exports = generateMarkdown;
